84ca1e742a2a129f4346f16953e23729dbb57d4e
appletail <csappletail@gmail.com>
2022-07-18 22:38:19 +0900

    1959. 두 개의 숫자열 풀이

--- SWEA 문제풀이.py
+++ SWEA 문제풀이.py
@@ -232,7 +232,7 @@
 # 1946 간단한 압축 풀기
 # 주소: https://url.kr/bx64md
 
-# 문제
+# [문제]
 # 원본 문서는 너비가 10인 여러 줄의 문자열로 이루어져 있다.
 # 문자열은 마지막 줄을 제외하고 빈 공간 없이 알파벳으로 채워져 있고 마지막 줄은 왼쪽부터 채워져 있다.
 # 이 문서를 압축한 문서는 알파벳과 그 알파벳의 연속된 개수로 이루어진 쌍들이 나열되어 있다. (예 : A 5    AAAAA)
@@ -318,17 +318,116 @@
 
 
 
+# 1959. 두 개의 숫자열
+# 주소: https://url.kr/bx64md
+
+# [문제]
+# N 개의 숫자로 구성된 숫자열 Ai (i=1~N) 와 M 개의 숫자로 구성된 숫자열 Bj (j=1~M) 가 있다.
+# 아래는 N =3 인 Ai 와 M = 5 인 Bj 의 예이다.
+
+# Ai 나 Bj 를 자유롭게 움직여서 숫자들이 서로 마주보는 위치를 변경할 수 있다.
+# 단, 더 긴 쪽의 양끝을 벗어나서는 안 된다.
+ 
+# 서로 마주보는 숫자들을 곱한 뒤 모두 더할 때 최댓값을 구하라.
+# 위 예제의 정답은 아래와 같이 30 이 된다.
+ 
+
+# [제약 사항]
+# N 과 M은 3 이상 20 이하이다.
 
 
+# [입력]
+# 가장 첫 줄에는 테스트 케이스의 개수 T가 주어지고, 그 아래로 각 테스트 케이스가 주어진다.
+# 각 테스트 케이스의 첫 번째 줄에 N 과 M 이 주어지고,
+# 두 번째 줄에는 Ai,
+# 세 번째 줄에는 Bj 가 주어진다.
 
 
+# [출력]
+# 출력의 각 줄은 '#t'로 시작하고, 공백을 한 칸 둔 다음 정답을 출력한다.
+# (t는 테스트 케이스의 번호를 의미하며 1부터 시작한다.)
 
 
+# [제출한 답1]
+
+# T = int(input())
+
+# for i in range(1, T + 1):
+
+#     N, M = map(int,input().split())  # N과 M은 range로 사용할 예정
+#     Ai = list(map(int, input().split()))
+#     Bj = list(map(int, input().split()))
+
+#     if M >= N :
+#         a = M - N
+#         b = 0
+#         d = []
+        
+#         for j in range(0, a + 1):
+#             for k in range(0, N):
+#                 c = int(Ai[k]) * (Bj[k + j])
+#                 b += c
+#             d.append(b)
+#             b = 0
+#         print(f'#{i} {max(d)}')
+
+#     else:
+#         a = N - M
+#         b = 0
+#         d = []
+        
+#         for j in range(0, a + 1):
+#             for k in range(0, M):
+#                 c = int(Bj[k]) * (Ai[k + j])
+#                 b += c
+#             d.append(b)
+#             b = 0
+#         print(f'#{i} {max(d)}')
 
 
+# [제출한 답2]
+# T = int(input())
+
+# for i in range(1, T + 1):
+
+#     N, M = map(int,input().split())  # N과 M은 range로 사용할 예정
+#     Ai = list(map(int, input().split()))
+#     Bj = list(map(int, input().split()))
+
+#     if M < N :
+#         M, N = N, M
+#         Ai, Bj = Bj, Ai
+
+#     a = M - N
+#     b = 0
+#     d = []
+    
+#     for j in range(0, a + 1):
+#         for k in range(0, N):
+#             c = int(Ai[k]) * (Bj[k + j])
+#             b += c
+#         d.append(b)
+#         b = 0
+#     print(f'#{i} {max(d)}')
 
 
-
+# [다른 답]
+# for i in range(int(input())):
+#     cnt=0
+#     a,b=list(map(int,input().split()))
+#     l=list(map(int,input().split()))
+#     k=list(map(int,input().split()))
+#     if a>b:
+#         a,b=b,a
+#         l,k=k,l
+#     o=0
+#     for j in range(b-a+1):
+#         p=0
+#         for t in range(a):
+#             p+=l[t]*k[t+j]
+#         if o<p:
+#             o=p
+#     print(f'#{i+1} {o}')
 
 
 
