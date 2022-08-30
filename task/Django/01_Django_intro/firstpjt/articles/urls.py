fcc120a8677216798bd22821eb1b11d3a99c96c9
appletail <csappletail@gmail.com>
2022-08-30 15:56:38 +0900

    장고 실습 1일차

---
+++ task/Django/01_Django_intro/firstpjt/articles/urls.py
@@ -0,0 +1,28 @@
+"""firstpjt URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    # articles
+    path('index/', views.index),
+    path('greeting/', views.greeting), # Django는 마지막에 ','를 붙이도록 권장함
+    path('dinner/', views.dinner),
+    path('throw/', views.throw),
+    path('catch/', views.catch),
+    path('hello/<str:name>/', views.hello), # '<>'가 들어가면 변수역할 'str:'안적어도 문자열이 기본값임
+]
