b6ee47e2dc4b83a645aade44a2d09e59b8823ebf
appletail <csappletail@gmail.com>
2022-11-03 16:39:37 +0900

    vue 3

---
+++ task/vue/3차/q4/src/components/Finaltax.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>결정세액: {{ taxCalculated - taxReduction > 0 ? taxCalculated - taxReduction : 0}} 만원</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Finaltax',
+  props: {
+    taxCalculated: Number,
+    taxReduction: Number
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ task/vue/3차/q4/src/components/Income.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div>
+      <label for="salary">연봉 입력 (만원): </label>
+      <input type="number" id="salary" v-model.number="salary">
+      <br>
+      <label for="tax-reduction">세액감면액 (만원): </label>
+      <input type="number" id="tax-reduction" v-model.number="taxReduction">
+      <hr>
+      <h2>종합소득금액 : {{ salary }} 만원</h2>
+      <h2>종합소득공제 : (-) 150 만원</h2>
+      <h2>과세표준 : {{ taxBase }} 만원</h2>
+    </div>
+    <hr>
+    <Taxrate 
+      :tax-base='taxBase'
+      :tax-reduction='taxReduction'
+    />
+  </div>
+</template>
+
+<script>
+import Taxrate from '@/components/Taxrate'
+
+export default {
+  name: 'Income',
+  components: {
+    Taxrate
+  },
+  data: function () {
+    return {
+      salary: 0,
+      taxReduction: 0,
+    }
+  },
+  computed: {
+    taxBase: function () {
+      return this.salary - 150 > 0 ? this.salary - 150 : 0
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
