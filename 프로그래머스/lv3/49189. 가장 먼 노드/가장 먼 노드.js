5f4af9f5dfbabef1cdced5de7ff094cde1b78034
Jangrammer <109320041+appletail@users.noreply.github.com>
2023-07-18 16:09:41 +0900

    [level 3] Title: 가장 먼 노드, Time: 355.20 ms, Memory: 65.9 MB -BaekjoonHub

--- 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.js
+++ 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -1,67 +1,33 @@
-class NodeQueue {
-    constructor(data) {
-        this.data = data
-        this.next = null
-    }
-}
+function solution(n, edges) {
+    // make adjacent list
+    const adjList = edges.reduce((G, [from, to]) => {
+        G[from] = (G[from] || []).concat(to);
+        G[to] = (G[to] || []).concat(from);
+        return G;
+    }, {});
 
-class Queue {
-    constructor() {
-        this.front = null
-        this.rear = null
-        this.size = 0
-    }
-    
-    isEmpty() {
-        return !this.size
-    }
-    
-    enqueue(data) {
-        const node = new NodeQueue(data)
-        if (this.isEmpty()) {this.front = node}
-        else {this.rear.next = node}
-        this.rear = node
-        this.size++
-    }
-    
-    dequeue() {
-        if (this.isEmpty()) {return false}
-        const value = this.front.data
-        this.front = this.front.next
-        this.size--
-        if (this.isEmpty()) {this.rear = null}
-        
-        return value
-    }
-}
+    // do BFS
+    const queue = [1];
+    const visited = {1: true};
+    const dist = {1: 0};
+    while(queue.length > 0) {
+        const node = queue.shift();
 
-function solution(n, edge) {
-    const graph = {}
-    for (i of edge) {
-        const [de, ar] = i
-        if (graph[de]) {graph[de].push(ar)}
-        else {graph[de] = [ar]}
-        if (graph[ar]) {graph[ar].push(de)}
-        else {graph[ar] = [de]}
-    }
-    
-    const visited = Array.from(Array(n + 1), () => 0)
-    visited[1] = 1
-    const q = new Queue()
-    q.enqueue(1)
-    let answer = [0, 0]  // 최대거리, 갯수
-    while (!q.isEmpty()) {
-        const v = q.dequeue()
-        if (visited[v] > answer[0]) {answer = [visited[v], 1]}
-        else if (visited[v] === answer[0]) {answer[1]++}
-        
-        for (node of graph[v]) {
-            if (!visited[node]) {
-                q.enqueue(node)
-                visited[node] = visited[v] + 1
-            }
+        if (adjList[node]) {
+            adjList[node].forEach(n => {
+                if (!visited[n]) {
+                    queue.push(n);
+                    visited[n] = true;
+                    const d = dist[node] + 1;
+                    if (dist[n] == undefined || d < dist[n]) {
+                        dist[n] = d;
+                    }
+                }
+            });
         }
     }
 
-    return answer[1]
+    const dists = Object.values(dist);
+    const maxDist = Math.max(...dists);
+    return dists.filter(d => d == maxDist).length;
 }
