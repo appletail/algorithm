6b7ea3b034979c20794ba2ccd0a786b3aa856f92
Jangrammer <109320041+appletail@users.noreply.github.com>
2023-07-18 15:25:41 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.52 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,36 +1,63 @@
-function solution(maps) {
-
-    var yLen = maps.length - 1;
-    var xLen = maps[0].length - 1;
-
-    var queue = [[0, 0]];
-
-    var visited = Array.from(new Array(maps.length), () => new Array(maps[0].length).fill(false));
-
-    var result = 0;
-
-    while (queue.length) {
-        result++;
-        var size = queue.length;
-        for (var i = 0; i < size; i++) {
-            var point = queue.shift();
-            var curY = point[0];
-            var curX = point[1];
-
-            if (visited[curY][curX]) continue;
-
-            maps[curY][curX] = 0;
-
-            visited[curY][curX] = true;
-
-            if (curY === yLen && curX === xLen) return result;
-
-            if (curY < yLen && maps[curY + 1][curX] === 1) queue.push([curY + 1, curX])
-            if (curX < xLen && maps[curY][curX + 1] === 1) queue.push([curY, curX + 1])
-            if (curY > 0 && maps[curY - 1][curX] === 1) queue.push([curY - 1, curX])
-            if (curX > 0 && maps[curY][curX - 1] === 1) queue.push([curY, curX - 1])
-        }
+class NodeQueue {
+    constructor(data) {
+        this.data = data
+        this.next = null
     }
-
-    return -1;
 }
+class Queue {
+    constructor() {
+        this.front = null
+        this.rear = null
+        this.size = 0
+    }
+    isEmpty() {
+        return !this.size
+    } 
+    enqueue(data) {
+        const node = new NodeQueue(data)
+        if (this.size === 0) {this.front = node}
+        else {this.rear.next = node}
+        this.rear = node
+        this.size++
+    }
+    dequeue() {
+        if (this.size === 0) {return false}
+        const value = this.front.data
+        this.front = this.front.next
+        this.size--
+        if (this.size === 0) {this.rear = null}
+        return value
+    }
+}
+
+
+function bfs(maps) {
+    const n = maps.length;
+    const m = maps[0].length;
+    const visited = Array.from(Array(n), () => Array(m).fill(0))
+    const dt = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+    const q = new Queue()
+    q.enqueue([0, 0])
+    
+    while (!q.isEmpty()) {
+        const v = q.dequeue()
+        if (v[0] === n - 1 && v[1] === m -1) {
+            return visited[v[0]][v[1]] + 1
+        }
+        for (const i of dt) {
+            const nr = v[0] + i[0]
+            const nc = v[1] + i[1]
+            if (0 <= nr && nr < n && 0 <= nc && nc < m) {
+                if (!visited[nr][nc] && !!maps[nr][nc]) {
+                    visited[nr][nc] = visited[v[0]][v[1]] + 1
+                    q.enqueue([nr, nc])
+                }
+            }
+        }        
+    }
+    return -1
+}
+
+function solution(maps) {
+    return bfs(maps);
+} 
