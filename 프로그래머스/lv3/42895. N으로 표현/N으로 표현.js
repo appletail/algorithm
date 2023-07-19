88b580cbaf12361d609aca07c32e1923f2cbef6e
Jangrammer <109320041+appletail@users.noreply.github.com>
2023-07-19 13:48:28 +0900

    [level 3] Title: N으로 표현, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv3/42895. N으로 표현/N으로 표현.js
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.js
@@ -1,25 +1,20 @@
 function solution(N, number) {
-    var answer = 0;
-    var use = Array.from(new Array(9), () => new Set());
-    if(N==number) return 1;
-    else {
-        use.forEach((element, index) => {
-            if (index !== 0) element.add(Number(String(N).repeat(index)));
-        });
-        for(var i=1 ; i<=8 ; ++i) {
-            for(var j=1 ; j<i ; ++j) {
-                for(var first of use[j]) {
-                    for(var second of use[i-j]) {
-                        use[i].add(first+second);
-                        use[i].add(first-second);
-                        use[i].add(first*second);
-                        use[i].add(first/second);
-                    }
-                } 
+    const memo = new Array(9).fill(0).map(() => new Set())
+    if (N == number) return 1
+    memo.forEach((elem, idx) => elem.add(Number(String(N).repeat(idx))))
+    
+    for (let i = 1; i < 9; i++) {
+        for (let j = 1; j < i; j++) {
+            for (let A of memo[j]) {
+                for (let B of memo[i - j]) {
+                    memo[i].add(A + B)
+                    memo[i].add(A - B)
+                    memo[i].add(A * B) 
+                    memo[i].add(A / B)
+                }
             }
-            if(use[i].has(number)) return i;
         }
-        return -1;
+        if (memo[i].has(number)) return i
     }
-    return answer;
+    return -1
 }
