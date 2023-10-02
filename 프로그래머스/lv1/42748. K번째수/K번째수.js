d2a3cbeb3d855d994d4251f6c6a87e9dbb1bd740
Jangrammer <109320041+appletail@users.noreply.github.com>
2023-10-02 21:52:23 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -0,0 +1,9 @@
+function solution(array, commands) {
+    var answer = [];
+    commands.forEach((command) => {
+        const sliced = array.slice(command[0] - 1, command[1])
+        sliced.sort((a, b) => a - b)
+        answer.push(sliced[command[2] - 1])
+    })
+    return answer;
+}
