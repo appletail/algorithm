35f9d95bcc34a5a92b72af5e609de7dfc3188497
Jangrammer <109320041+appletail@users.noreply.github.com>
2023-10-03 22:23:18 +0900

    [level 2] Title: 더 맵게, Time: 1696.73 ms, Memory: 51.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,17 @@
+from heapq import heappush, heappop, heapify
+
+def solution(scoville, K):
+    answer = 0
+    heapify(scoville)
+    
+    while scoville[0] < K and len(scoville) >= 2:
+        food1 = heappop(scoville) 
+        food2 = heappop(scoville)
+        heappush(scoville, food1 + food2 * 2)
+        answer += 1
+        
+    if not scoville or scoville[0] < K:
+        answer = -1
+    
+    
+    return answer
