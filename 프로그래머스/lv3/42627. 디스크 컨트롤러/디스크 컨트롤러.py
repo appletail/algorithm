35414361276d5465c06c888bfd6d8f580db9954c
Jangrammer <109320041+appletail@users.noreply.github.com>
2023-10-04 15:32:23 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,30 +1,47 @@
+# import heapq
+
+# def solution(jobs):
+#     answer = 0
+#     length = len(jobs)
+#     jobs = [[spend, start] for start, spend in jobs]
+#     heapq.heapify(jobs)
+    
+#     time = 0
+#     while jobs:
+#         tmps = []
+        
+#         while jobs:
+#             spend, start = heapq.heappop(jobs)
+#             if start > time:
+#                 tmps.append([spend, start])
+#             else:
+#                 break
+
+#         if start <= time:
+#             answer += time - start + spend
+#             time += spend
+#         else:
+#             time += 1
+            
+#         while tmps:
+#             tmp = tmps.pop()
+#             heapq.heappush(jobs, tmp)
+    
+#     return answer // length
 import heapq
+from collections import deque
 
 def solution(jobs):
-    answer = 0
-    length = len(jobs)
-    jobs = [[spend, start] for start, spend in jobs]
-    heapq.heapify(jobs)
-    
-    time = 0
-    while jobs:
-        tmps = []
-        
-        while jobs:
-            spend, start = heapq.heappop(jobs)
-            if start > time:
-                tmps.append([spend, start])
-            else:
-                break
-
-        if start <= time:
-            answer += time - start + spend
-            time += spend
-        else:
-            time += 1
-            
-        while tmps:
-            tmp = tmps.pop()
-            heapq.heappush(jobs, tmp)
-    
-    return answer // length
+    tasks = deque(sorted([(x[1], x[0]) for x in jobs], key=lambda x: (x[1], x[0])))
+    q = []
+    heapq.heappush(q, tasks.popleft())
+    current_time, total_response_time = 0, 0
+    while len(q) > 0:
+        dur, arr = heapq.heappop(q)
+        current_time = max(current_time + dur, arr + dur)
+        total_response_time += current_time - arr
+        while len(tasks) > 0 and tasks[0][1] <= current_time:
+            heapq.heappush(q, tasks.popleft())
+        if len(tasks) > 0 and len(q) == 0:
+            heapq.heappush(q, tasks.popleft())
+    return total_response_time // len(jobs)
