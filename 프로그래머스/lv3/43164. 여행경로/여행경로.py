84dd45f759a2fac9f15fb37c0b624a7e1947b7bf
Jangrammer <109320041+appletail@users.noreply.github.com>
2023-10-06 00:51:17 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/43164. 여행경로/여행경로.py
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.py
@@ -1,38 +1,70 @@
-airRoute = []
-def dfs(tickets, used, i, s, answer, L):
-    global airRoute
+# airRoute = []
+# def dfs(tickets, used, i, s, answer, L):
+#     global airRoute
     
-    if airRoute:
-        return
+#     if airRoute:
+#         return
 
-    if i == L:
-        airRoute = answer
-        return
+#     if i == L:
+#         airRoute = answer
+#         return
 
-    depart = s.pop()
+#     depart = s.pop()
     
-    for idx in range(L):
-        d, a = tickets[idx]
-        if not used[idx] and depart == d:
-            s.append(a)
-            answer.append(a)
-            used[idx] = 1
-            dfs(tickets, used, i + 1, s, answer, L)
-            if airRoute:
-                break
-            answer.pop()
-            used[idx] = 0
+#     for idx in range(L):
+#         d, a = tickets[idx]
+#         if not used[idx] and depart == d:
+#             s.append(a)
+#             answer.append(a)
+#             used[idx] = 1
+#             dfs(tickets, used, i + 1, s, answer, L)
+#             if airRoute:
+#                 break
+#             answer.pop()
+#             used[idx] = 0
     
         
 
+# def solution(tickets):
+#     global airRoute
+    
+#     answer = ["ICN"]
+#     L = len(tickets)
+#     used = [0] * (L + 1)
+#     used[-1] = 1
+#     tickets = sorted(tickets)
+#     dfs(tickets, used, 0, ["ICN"], answer, L)
+    
+#     return airRoute
+
+from collections import defaultdict
+
+
+def dfs(graph, n, depart, route):
+    if len(route) == n + 1:
+        return route
+    
+    for idx, arrive in enumerate(graph[depart]):
+        graph[depart].pop(idx)
+        route.append(arrive)
+        answer = dfs(graph, n, arrive, route)
+        
+        if answer: 
+            return answer
+        
+        route.pop()
+        graph[depart].insert(idx, arrive)
+        
+
 def solution(tickets):
-    global airRoute
+    graph = defaultdict(list)
+
+    for ticket in tickets:
+        graph[ticket[0]].append(ticket[1])
+        
+    for value in graph.values():
+        value.sort()
     
-    answer = ["ICN"]
-    L = len(tickets)
-    used = [0] * (L + 1)
-    used[-1] = 1
-    tickets = sorted(tickets)
-    dfs(tickets, used, 0, ["ICN"], answer, L)
-    
-    return airRoute
+    answer = dfs(graph, len(tickets), 'ICN', ['ICN'])
+
+    return answer
