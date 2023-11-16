2d0e7c3be7d648916da611dd61bbb7d9fb11d332
Jangrammer <109320041+appletail@users.noreply.github.com>
2023-11-16 19:39:41 +0900

    [level 2] Title: 피보나치 수, Time: 84.15 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,9 +1,6 @@
 def solution(n):
-    answer = 0
-
     first, second = 0, 1
     for _ in range(n - 1):
-        answer = first + second
-        first, second = second, answer
+        first, second = second, first + second
 
-    return answer % 1_234_567
+    return second % 1_234_567
