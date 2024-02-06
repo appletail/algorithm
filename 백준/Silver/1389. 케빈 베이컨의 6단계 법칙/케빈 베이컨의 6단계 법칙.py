ef2c5eb85865827c1c31fad4fbf694e27651d679
Jangrammer <109320041+appletail@users.noreply.github.com>
2024-02-06 22:57:12 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 144 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -7,13 +7,15 @@ N, M = map(int, input().split())
 G = [[0] * (N + 1) for _ in range(N + 1)]
 for _ in range(M):
     a, b = map(int, input().split())
-    G[a][b], G[b][a] = True, True
+    G[a][b], G[b][a] = 1, 1
 
+minV = 1e10
+answer = 0
 sumV = [0] * (N + 1)
-
 for a in range(1, N + 1):
     q = deque([a])
     visited = [0] * (N + 1)
+    visited[a] = 1
     while q:
         v = q.popleft()
         for b in range(1, N + 1):
@@ -21,13 +23,9 @@ for a in range(1, N + 1):
                 visited[b] = visited[v] + 1
                 q.append(b)
 
-    sumV[a] = sum(visited) - visited[a]
-
-minV = 1e10
-answer = 0
-for i in range(1, N + 1):
-    if sumV[i] < minV:
-        minV = sumV[i]
-        answer = i
+    sumV[a] = sum(visited)
+    if sumV[a] < minV:
+        minV = sumV[a]
+        answer = a
 
 print(answer)
