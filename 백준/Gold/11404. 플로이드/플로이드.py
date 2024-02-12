3428d62b5c86f19403edc827d363add794ad0dbf
Jangrammer <109320041+appletail@users.noreply.github.com>
2024-02-12 21:10:34 +0900

    [Gold IV] Title: 플로이드, Time: 600 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -3,14 +3,15 @@ input = sys.stdin.readline
 
 n = int(input())
 m = int(input())
+INF = 1e10
+nodes = [[INF] * n for _ in range(n)]
+for i in range(n):
+    nodes[i][i] = 0
 
-nodes = [[1e10] * n for _ in range(n)]
 for _ in range(m):
     a, b, c = map(int, input().split())
     nodes[a - 1][b - 1] = min(nodes[a - 1][b - 1], c)
 
-for i in range(n):
-    nodes[i][i] = 0
 
 for k in range(n):
     for s in range(n):
@@ -18,9 +19,4 @@ for k in range(n):
             nodes[s][e] = min(nodes[s][k] + nodes[k][e], nodes[s][e])
 
 for i in range(n):
-    for j in range(n):
-        if nodes[i][j] == 1e10:
-            nodes[i][j] = 0
-
-for i in range(n):
-    print(*nodes[i])
+    print(*[0 if x == INF else x for x in nodes[i]])
