a527c94f419eeaf1328c6872a7371351e1bd6c15
Jangrammer <109320041+appletail@users.noreply.github.com>
2024-02-12 21:15:02 +0900

    [Gold IV] Title: 플로이드, Time: 228 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,22 +1,30 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-m = int(input())
-INF = 1e10
-nodes = [[INF] * n for _ in range(n)]
-for i in range(n):
-    nodes[i][i] = 0
 
-for _ in range(m):
-    a, b, c = map(int, input().split())
-    nodes[a - 1][b - 1] = min(nodes[a - 1][b - 1], c)
+def solution():
+    n = int(input())
+    m = int(input())
+
+    INF = 1000000000
+    distance = [[INF] * (n + 1) for _ in range(n + 1)]
+
+    for i in range(1, n + 1):
+        distance[i][i] = 0
+
+    for _ in range(m):
+        a, b, c = map(int, input().split())
+        if distance[a][b] > c:
+            distance[a][b] = c
+
+    for k in range(1, n + 1):
+        for i in range(1, n + 1):
+            for j in range(1, n + 1):
+                if distance[i][j] > distance[i][k] + distance[k][j]:
+                    distance[i][j] = distance[i][k] + distance[k][j]
+
+    for i in range(1, n + 1):
+        print(*[x if x != INF else 0 for x in distance[i][1:]])
 
 
-for k in range(n):
-    for s in range(n):
-        for e in range(n):
-            nodes[s][e] = min(nodes[s][k] + nodes[k][e], nodes[s][e])
-
-for i in range(n):
-    print(*[0 if x == INF else x for x in nodes[i]])
+solution()
