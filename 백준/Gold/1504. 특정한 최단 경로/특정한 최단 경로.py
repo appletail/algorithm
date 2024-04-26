0826716cf16fb423fb75ea60e37b15c87afe7a34
Jangrammer <109320041+appletail@users.noreply.github.com>
2024-04-26 10:49:57 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 772 ms, Memory: 68368 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
@@ -2,7 +2,7 @@ import sys
 input = sys.stdin.readline
 import heapq
 
-def dijkstra(s, e):
+def dijkstra(s):
     distance = [1e10] * (N + 1)
     distance[s] = 0
 
@@ -18,7 +18,7 @@ def dijkstra(s, e):
                     distance[nodeTo] = newDist
                     heapq.heappush(heap, [newDist, nodeTo])
 
-    return distance[e]
+    return distance
 
 N, E = map(int, input().split())
 graph = [[] for _ in range(N + 1)]
@@ -28,7 +28,10 @@ for _ in range(E):
     graph[c].append([r, d])
 v1, v2 = map(int, input().split())
 
-case1 = dijkstra(1, v1) + dijkstra(v1, v2) + dijkstra(v2, N)
-case2 = dijkstra(1, v2) + dijkstra(v2, v1) + dijkstra(v1, N)
+dijk1 = dijkstra(v1)
+dijk2 = dijkstra(v2)
+
+case1 = dijk1[1] + dijk1[v2] + dijk2[N]
+case2 = dijk2[1] + dijk2[v1] + dijk1[N]
 
 print(-1 if case1 >= 1e10 and case2 >= 1e10 else min(case1, case2))
