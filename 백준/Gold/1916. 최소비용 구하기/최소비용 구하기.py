4012cc5cc57fbe954473844c7709258ce4708f7e
Jangrammer <109320041+appletail@users.noreply.github.com>
2024-09-12 16:05:17 +0900

    [Gold V] Title: 최소비용 구하기, Time: 280 ms, Memory: 57372 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -1,35 +1,30 @@
 import sys
-from heapq import heappop, heappush
-
-
-def dijk(graph, start):
-    total_costs = [1e10] * (N + 1)
-    total_costs[start] = 0
-    queue = []
-    heappush(queue, (start, total_costs[start]))
-    while queue:
-        node, cost = heappop(queue)
-
-        if total_costs[node] < cost:
-            continue
-
-        for next_node, next_cost in graph[node]:
-            new_cost = total_costs[node] + next_cost
-            if new_cost < total_costs[next_node]:
-                total_costs[next_node] = new_cost
-                heappush(queue, (next_node, new_cost))
-
-    return total_costs
-
-
 input = sys.stdin.readline
+import heapq
+
 N = int(input())
 M = int(input())
 
-GRAPH = [[] for _ in range(N + 1)]
+graph = [[] for _ in range(N+1)]
 for _ in range(M):
-    DEPART, ARRIVE, COST = map(int, input().split())
-    GRAPH[DEPART].append([ARRIVE, COST])
-START, END = map(int, input().split())
+    depart, arrive, cost = map(int, input().split())
+    graph[depart].append((arrive, cost))
 
-print(dijk(GRAPH, START)[END])
+A, B = map(int, input().split())
+
+INF = 1e10
+costs = [INF] * (N+1)
+costs[A] = 0
+heap = []
+heapq.heappush(heap, (costs[A], A))
+while heap:
+    cur_cost, depart = heapq.heappop(heap)
+    if costs[depart] < cur_cost:
+        continue
+    for arrive, cost in graph[depart]:
+        new_cost = cost + cur_cost
+        if costs[arrive] > new_cost:
+            costs[arrive] = new_cost
+            heapq.heappush(heap, (new_cost, arrive))
+
+print(costs[B])
