77d0fdac14fd2744292ffcf5d5c1ac85aa3287ee
Jangrammer <109320041+appletail@users.noreply.github.com>
2025-01-20 23:18:32 +0900

    [Gold IV] Title: 문자열 폭발, Time: 260 ms, Memory: 42956 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -1,23 +1,17 @@
 import sys
 input = sys.stdin.readline
-string = input().strip()
-bomb = input().strip()
 
-stack = []
-for cha in string:
-    stack.append(cha)
-    while True:
-        if len(stack) < len(bomb):
-            break
-        flag = False
-        for i in range(len(bomb)):
-            if stack[-(i+1)] != bomb[-(i+1)]:
-                flag = True
-                break
-        if flag:
-            break
-        else:
-            for _ in range(len(bomb)):
+def solution(string, bomb):
+    stack = []
+    bombLength = len(bomb)
+    for cha in string:
+        stack.append(cha)
+        if stack[-bombLength:] == bomb:
+            for _ in range(bombLength):
                 stack.pop()
 
-print("".join(stack) if stack else "FRULA")
+    return "".join(stack) if stack else "FRULA"
+
+string = input().strip()
+bomb = list(input().strip())
+print(solution(string, bomb))
