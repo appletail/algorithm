ea5869e9dea0ca208e558e38d8de3d0881102794
Jangrammer <109320041+appletail@users.noreply.github.com>
2025-01-22 16:36:08 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 144 ms, Memory: 45916 KB -BaekjoonHub

---
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+def hideAndSeek(cur, dest):
+    visited = [[0, 0] for _ in range(100001)]
+    q = deque()
+    visited[cur][1] = 1
+    q.append(cur)
+    while q:
+        v = q.popleft()
+        if v == dest:
+            return visited[v]
+
+        for dv in [(v+1), (v-1), (v*2)]:
+            if 0 <= dv <= 100_000:
+                if not visited[dv][1] or visited[dv][0] > visited[v][0]+1:
+                    visited[dv][0] = visited[v][0] + 1
+                    visited[dv][1] = visited[v][1]
+                    q.append(dv)
+                else:
+                    if visited[dv][0] == visited[v][0]+1:
+                        visited[dv][1] += visited[v][1]
+
+N, K = map(int, input().split())
+for answer in hideAndSeek(N, K):
+    print(answer)
