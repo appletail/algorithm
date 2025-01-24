79876a9ee7e1d9b1130ec8e3c8897a6d01c233f0
Jangrammer <109320041+appletail@users.noreply.github.com>
2025-01-24 16:30:55 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 220 ms, Memory: 65836 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
@@ -2,32 +2,37 @@ import sys
 input = sys.stdin.readline
 import heapq
 
-n = int(input())
-m = int(input())
-graph = [[] for _ in range(n+1)]
+def mine():
+    n = int(input())
+    m = int(input())
+    graph = [[] for _ in range(n+1)]
 
-for _ in range(m):
-    s, e, w = map(int, input().split())
-    graph[s].append([e, w])
+    for _ in range(m):
+        s, e, w = map(int, input().split())
+        graph[s].append([e, w])
 
-depart, arrive = map(int, input().split())
+    depart, arrive = map(int, input().split())
 
-INF = 1e10
-distance = [[INF, ''] for _ in range(n + 1)]
-q = []
-heapq.heappush(q, (0, depart, f'{depart}'))
-distance[depart] = [0, f'{depart}']
-while q:
-    dist, s, route = heapq.heappop(q)
-    if distance[s][0] < dist:
-        continue
+    INF = 1e10
+    distance = [[INF, ''] for _ in range(n + 1)]
+    q = []
+    distance[depart] = [0, f'{depart}']
+    heapq.heappush(q, (0, depart, f'{depart}'))
+    while q:
+        dist, s, route = heapq.heappop(q)
+        if s == arrive:
+            break
+        if distance[s][0] < dist:
+            continue
 
-    for e, w in graph[s]:
-        if dist + w < distance[e][0]:
-            distance[e] = [dist + w, f'{route},{e}']
-            heapq.heappush(q, (dist + w, e, f'{route},{e}'))
+        for e, w in graph[s]:
+            if dist + w < distance[e][0]:
+                distance[e] = [dist + w, f'{route},{e}']
+                heapq.heappush(q, (dist + w, e, f'{route},{e}'))
 
-print(distance[arrive][0])
-route = distance[arrive][1].split(',')
-print(len(route))
-print(*route)
+    print(distance[arrive][0])
+    route = distance[arrive][1].split(',')
+    print(len(route))
+    print(*route)
+
+mine()
