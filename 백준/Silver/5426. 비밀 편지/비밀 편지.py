dc3db05fc4a0e73c279b1ca2fe36613f530bc109
Jangrammer <109320041+appletail@users.noreply.github.com>
2025-02-05 16:02:17 +0900

    [Silver V] Title: 비밀 편지, Time: 196 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/5426. 비밀 편지/비밀 편지.py
@@ -0,0 +1,10 @@
+TC = int(input())
+for _ in range(TC):
+    message = input()
+    n = int(len(message) ** 0.5)
+    cur = n - 1
+    while cur >= 0:
+        for i in range(cur, len(message), n):
+            print(message[i], end='')
+        cur -= 1
+    print()
