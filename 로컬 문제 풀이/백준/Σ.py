196fdc91496c36544349d463f28084fac7004351
appletail <csappletail@gmail.com>
2025-02-18 11:39:40 +0900

    사전 순 최대 공통 부분 수열, Σ

--- 로컬 문제 풀이/백준/Σ.py
+++ 로컬 문제 풀이/백준/Σ.py
@@ -1,6 +1,28 @@
-def solution():
-    M = int(input())
-    dices = [list(map(int, input().split())) for _ in range(M)]
-    print(dices)
+import sys
+from math import gcd
 
-solution()
+input = sys.stdin.readline
+
+def multi(base, power):
+    if power == 0:
+        return 1
+
+    tmp = multi(base, power // 2)
+    if power % 2:
+        return tmp * tmp * base % X
+    else:
+        return tmp * tmp % X
+
+M = int(input())
+dices = [list(map(int, input().split())) for _ in range(M)]
+X = 1_000_000_007
+
+answer = 0
+for n, s in dices:
+    cd = gcd(s, n)
+    s //= cd
+    n //= cd
+
+    answer += s * multi(n, X-2) % X
+
+print(answer % X)
