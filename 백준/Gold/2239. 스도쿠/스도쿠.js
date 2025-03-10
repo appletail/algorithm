e6c68c421ce7e3149a2a54e1a23afe4f3b79210d
Jangrammer <109320041+appletail@users.noreply.github.com>
2025-03-10 23:42:38 +0900

    [Gold IV] Title: 스도쿠, Time: 608 ms, Memory: 10804 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.js
+++ 백준/Gold/2239. 스도쿠/스도쿠.js
@@ -4,9 +4,9 @@ const input = fs.readFileSync(filePath).toString().trim().split("\n")
 
 const sudoku = input.map((v) => v.trim().split("").map(Number))
 
-const rows = Array.from(new Array(9), () => new Object())
-const columns = Array.from(new Array(9), () => new Object())
-const squares = Array.from(new Array(9), () => new Object())
+const rows = Array.from(new Array(9), () => new Array(10).fill(0))
+const columns = Array.from(new Array(9), () => new Array(10).fill(0))
+const squares = Array.from(new Array(9), () => new Array(10).fill(0))
 const blanks = []
 
 const findSquareIdx = (i, j) => {
@@ -17,39 +17,35 @@ for (let i = 0; i < 9; i++) {
   for (let j = 0; j < 9; j++) {
     if (sudoku[i][j] === 0) blanks.push([i, j])
     else {
-      rows[i][sudoku[i][j]] = true
-      columns[j][sudoku[i][j]] = true
-      squares[findSquareIdx(i, j)][sudoku[i][j]] = true
+      rows[i][sudoku[i][j]] = 1
+      columns[j][sudoku[i][j]] = 1
+      squares[findSquareIdx(i, j)][sudoku[i][j]] = 1
     }
   }
 }
 
 const solve = (blankIdx) => {
-  if (blankIdx == blanks.length) return true
+  if (blankIdx == blanks.length) {
+    sudoku.forEach((v) => console.log(v.join("")))
+    process.exit(0)
+  }
   const [r, c] = blanks[blankIdx]
 
   for (let i = 1; i < 10; i++) {
     const row = rows[r]
     const column = columns[c]
     const square = squares[findSquareIdx(r, c)]
-    if (
-      !row.hasOwnProperty(i) &&
-      !column.hasOwnProperty(i) &&
-      !square.hasOwnProperty(i)
-    ) {
+    if (!row[i] && !column[i] && !square[i]) {
       sudoku[r][c] = i
-      row[i] = true
-      column[i] = true
-      square[i] = true
-      const isDone = solve(blankIdx + 1)
-      if (isDone) return true
-      delete row[i]
-      delete column[i]
-      delete square[i]
+      row[i] = 1
+      column[i] = 1
+      square[i] = 1
+      solve(blankIdx + 1)
+      row[i] = 0
+      column[i] = 0
+      square[i] = 0
       sudoku[r][c] = 0
     }
   }
 }
-
 solve(0)
-sudoku.forEach((v) => console.log(v.join("")))
