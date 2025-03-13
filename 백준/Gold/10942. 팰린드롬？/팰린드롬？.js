19cb82801b6ec77885c623c1ac7c9e6407bd00ee
Jangrammer <109320041+appletail@users.noreply.github.com>
2025-03-13 14:32:20 +0900

    [Gold IV] Title: 팰린드롬?, Time: 1592 ms, Memory: 201252 KB -BaekjoonHub

---
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.js
@@ -0,0 +1,32 @@
+const fs = require("fs")
+const filePath = process.platform === "linux" ? "dev/stdin" : "백준/input.txt"
+const input = fs.readFileSync(filePath).toString().split("\n")
+
+const N = Number(input[0])
+const nums = input[1].split(" ").map(Number)
+const M = Number(input[2])
+
+const dp = Array.from(Array(N), () => new Array(N).fill(0))
+
+for (let i = 0; i < N; i++) {
+  dp[i][i] = 1
+}
+
+for (let j = 0; j < N; j++) {
+  for (let i = 0; i < j; i++) {
+    let [S, E] = [i, j]
+    if (nums[S] !== nums[E]) {
+      continue
+    }
+    S += 1
+    E -= 1
+    if (S > E || dp[S][E]) dp[i][j] = 1
+  }
+}
+
+let answer = ""
+for (let i = 3; i < M + 3; i++) {
+  const [S, E] = input[i].split(" ").map(Number)
+  answer += `${dp[S - 1][E - 1]}\n`
+}
+console.log(answer)
