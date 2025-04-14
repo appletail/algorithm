a5bf8960601179161a06f8c9a73c0a9de46aff4e
Jangrammer <109320041+appletail@users.noreply.github.com>
2025-04-14 17:24:25 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.04 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.py
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.py
@@ -1,11 +1,8 @@
 def solution(n):
-    answer = 0
-    cnt = 1
-    
-    while cnt <= n:
+    answer = 0    
+    for _ in range(n):
         answer += 1
         while '3' in str(answer) or answer % 3 == 0:
             answer += 1
-        cnt += 1
         
     return answer
