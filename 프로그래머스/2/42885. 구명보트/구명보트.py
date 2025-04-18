545c807f2338e13eb4a15bbf26d64edda5af27e2
Jangrammer <109320041+appletail@users.noreply.github.com>
2025-04-18 14:23:11 +0900

    [level 2] Title: 구명보트, Time: 8.85 ms, Memory: 9.67 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -3,12 +3,11 @@ def solution(people, limit):
     people.sort()
     l = 0
     r = len(people) - 1
+    
     while l <= r:
         if people[l] + people[r] <= limit:
             l += 1
-            r -= 1
-            answer += 1
-        else:
-            r -= 1
-            answer += 1
+        r -= 1
+        answer += 1
+        
     return answer
